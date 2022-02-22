f394b899852fa872a356d8a46132dfd5d3988bec
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-02-22 23:57:49 +0200

    add solution to task two

--- 02. Time-Converter/solution.js
+++ 02. Time-Converter/solution.js
@@ -1,4 +1,39 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let daysElement = document.querySelector('#days');
+    let hoursElement = document.querySelector('#hours');
+    let minutesElement = document.querySelector('#minutes');
+    let secondsElement = document.querySelector('#seconds');
+
+    let buttonDays = document.querySelector('#daysBtn');
+
+    buttonDays.addEventListener('click', (e) => {
+        hoursElement.value = Number(daysElement.value) * 24;
+        minutesElement.value = Number(hoursElement.value) * 60;
+        secondsElement.value = Number(minutesElement.value) * 60;
+    })
+
+    let buttonHours = document.querySelector('#hoursBtn');
+
+    buttonHours.addEventListener('click', (e) => {
+        daysElement.value = (Number(hoursElement.value) / 24).toFixed(1);
+        minutesElement.value = Number(hoursElement.value) * 60;
+        secondsElement.value = Number(minutesElement.value) * 60;
+    })
+
+    let buttonMinutes = document.querySelector('#minutesBtn');
+
+    buttonMinutes.addEventListener('click', (e) => {
+        secondsElement.value = Number(minutesElement.value) * 60;
+        hoursElement.value = Number(minutesElement.value) / 60;
+        daysElement.value = (Number(hoursElement.value) / 24).toFixed(1);
+    })
+
+    let buttonSeconds = document.querySelector('#secondsBtn');
+
+    buttonSeconds.addEventListener('click', (e) => {
+        minutesElement.value = Number(secondsElement.value) / 60;
+        hoursElement.value = Number(minutesElement.value) / 60;
+        daysElement.value = (Number(hoursElement.value) / 24).toFixed(1);
+    })
 }
