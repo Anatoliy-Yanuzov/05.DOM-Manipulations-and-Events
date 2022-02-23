881d2a44cd7bf514d476c018f6a9e3849d55a322
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-02-23 14:23:04 +0200

    add solution

--- 03. Locked-Profile/app.js
+++ 03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    let allButtonsElements = document.querySelectorAll('button');
+
+    for (const button of allButtonsElements) {
+        button.addEventListener('click', (e) => {
+            let btn = e.target;
+            let currentUser = btn.parentNode;
+            let moreInformation = currentUser.getElementsByTagName('div')[0];
+            let lockStatus = currentUser.querySelector('input[type="radio"]:checked').value;
+
+            if (lockStatus === 'unlock') {
+                if (btn.textContent === 'Show more') {
+                    moreInformation.style.display = 'inline-block';
+                    btn.textContent = 'Hide it';
+                } else if (btn.textContent == 'Hide it') {
+                    moreInformation.style.display = 'none';
+                    btn.textContent = 'Show more';
+                }
+            }
+        });
+    }
 }
