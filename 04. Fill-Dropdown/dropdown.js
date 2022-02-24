5c8370dac4ebba6dfa6365d92f124cee3efee6af
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-02-25 00:43:17 +0200

    add solution

--- 04. Fill-Dropdown/dropdown.js
+++ 04. Fill-Dropdown/dropdown.js
@@ -1,10 +1,14 @@
 function addItem() {
+
     let textElement = document.querySelector('#newItemText');
     let valueElement = document.querySelector('#newItemValue');
 
     let createElementOption = document.createElement('option');
+    createElementOption.textContent = textElement.value + ' ' + valueElement.value;
+
     let menuElement = document.querySelector('#menu');
     menuElement.appendChild(createElementOption);
-    let optionElement = document.querySelector('#addTextAndValue');
-    optionElement.textContent = 'Hello'
+
+    textElement.value = '';
+    valueElement.value = '';
 }
